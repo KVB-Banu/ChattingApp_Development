875a4c2b2d95a97026c296204e8f0bc22bfa6508
KVB-Banu <109133371+KVB-Banu@users.noreply.github.com>
2022-11-23 16:22:32 +0530

    added image feature

--- MyfirstProject.java
+++ MyfirstProject.java
@@ -8,7 +8,8 @@ System.out.println("Chaanu Kutty");
 System.out.println("adding chatting feature");
 System.out.println("Viji"); 
 System.out.println("Added Emoji Features");
-System.out.println("Added Status Features");  
+System.out.println("Added Status Features"); 
+System.out.println("Added image Features");  
   
 }
 }
