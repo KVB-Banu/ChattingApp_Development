71160e2db7c61e35db4d3016a7f2383221c8076a
KVB-Banu <109133371+KVB-Banu@users.noreply.github.com>
2022-11-23 13:02:51 +0530

    added viji

--- MyfirstProject.java
+++ MyfirstProject.java
@@ -6,7 +6,7 @@ System.out.println("Hello");
 System.out.println("Banu");
 System.out.println("Chaanu Kutty");
 System.out.println("adding chatting feature");
- 
+System.out.println("Viji"); 
   System.out.println("Added Emoji Features");
 }
 }
